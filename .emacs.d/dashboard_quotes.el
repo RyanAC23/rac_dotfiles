0d1a0cd8276070a5fec545983ef0c19b6265f37a
ryan <ryanacorbin@gmail.com>
2020-11-29 20:48:16 -0800

    Added lots of quotes

--- .emacs.d/dashboard_quotes.el
+++ .emacs.d/dashboard_quotes.el
@@ -1,16 +1,32 @@
 ;; my quotes
 (setq dashboard-quote-list '(
-			     ;; Blood Meridian
-			     "\"If God meant to interfere in the degeneracy of mankind would he not have done so by now?\""
-			     "\"Ah priest. What can I ask of you that you've not already given?\""
-			     "\"Tobin Smiled. Every man in the company claims to have encountered that sootysouled rascal in some other place.\""
-			     ;; Shakespeare
-			     "\"Dost thou think, because thou art virtuous, there shall be no more cakes and ale?\""
-			     ;; people
-			     "\"Be an asset, not a liability.\" - Tim Kennedy\""
-			     "\"Unless you can think the way an evil person thinks, you're defenseless against them... they'll go places you can't imagine.\""
-			     "\"All of your excuses are Lies.\" - Jocko"
-			     "\"If I'm so smart, why am I losing?\" - Jocko"
-			     "\"The progenitor of Ares is the first dude that was so scary that him showing up to battle automatically made the enemy flee.\""
-			     "\"Men with rot in their hearts and resentment instead of inspiration cannot create beauty.\""
+;; Blood Meridian
+     "\"If God meant to interfere in the degeneracy of mankind would he not have done so by now?\""
+     "\"Ah priest. What can I ask of you that you've not already given?\""
+     "\"Tobin Smiled. Every man in the company claims to have encountered that sootysouled rascal in some other place.\""
+     ;; Nabokov
+     "\"Don't be too certain of learning the past from the lips of the present.\" - RLSK"
+     "\"These connections are a little too easy, and thus probably fake.\" - Nabokov, Gogol"
+     "\"I doubt whether you can give your telephone number without giving something of yourself.\" - Nabokov, Gogol"
+     "\"If parallel lines do not meet it is not because they cannot, but because they have other things to do.\" - Nabokov, Gogol"
+     ;; Shakespeare
+     "\"They have been at a great feast of languages, and stolen the scraps.\" - Love's Labour Lost"
+     "\"If we imagine no worse of them than they of themselves, they may pass for excellent men.\" - Midsummer Night's Dream"
+     "\"Dost thou think, because thou art virtuous, there shall be no more cakes and ale?\" - Twelfth Night"
+     "\"There are more things in heaven and earth, Horatio, than are dreamt of in your philosophy.\" - Hamlet"
+     "\"Conceit in weakest bodies strongest works.\" - Hamlet"
+     "\"O that men's ears should be to counsel deaf, but not to flattery.\" - Timon of Athens"
+     "\"Yet Brutus says, he was ambitious, and Brutus is an honorable man.\" - Julius Caesar"
+     "\"I cannot draw a cart nor eat dried oats; if it be a man's work, I'll do it.\" - King Lear"
+     "\"The man that did once sell the lion's skin while the beast lived, was killed with hunting him.\" - Henry V"
+     "\"Ah, what a sign it is of evil life, where death's approach seems so terrible!\" - 2 Henry VI"
+     ;; people
+     "\"Be an asset, not a liability.\" - Tim Kennedy"
+     "\"All of your excuses are Lies.\" - Jocko"
+     "\"If I'm so smart, why am I losing?\" - Jocko"
+     "\"The enemy always has a vote.\" - Jocko"
+     "\"Do you think the company senior executives want you to fail?\" - Jocko"
+     "\"Anything you can't carry 24 hrs/day is gone on the next chopper.\""
+     "\"The progenitor of Ares is the first dude that was so scary that him showing up to battle automatically made the enemy flee.\""
+     "\"Men with rot in their hearts and resentment instead of inspiration cannot create beauty.\""
 ))
