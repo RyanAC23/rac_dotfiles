f7f9020635a44562dd08bc6ba5ce1aa3b1b116c4
ryan <ryanacorbin@gmail.com>
2022-12-16 18:59:34 -0800

    CHK: Login wallpaper override works.

--- setup_docs/2022_scripts/backgrounds.sh
+++ setup_docs/2022_scripts/backgrounds.sh
@@ -20,13 +20,13 @@ SetLoginBG(){
     BG_LOCATION="$BG_DIR/login.png"
     CONFIG_FILE="/usr/share/glib-2.0/schemas/30_ubuntu-mate.gschema.override"
 
-    # if [ -f $BG_LOCATION ]; then
-    #  	sudo rm $BG_LOCATION
-    # fi
-    # sudo wget $LOGIN -P $BG_DIR
+    if [ -f $BG_LOCATION ]; then
+     	sudo rm $BG_LOCATION
+    fi
+    sudo wget $LOGIN -P $BG_DIR
     sudo sed -in "s,\/usr.*\(png\|jpg\),"$BG_LOCATION",g" $CONFIG_FILE
-    # sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
-    cat $CONFIG_FILE | grep "background="
+    sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+    # cat $CONFIG_FILE | grep "background="
 }
 
 
